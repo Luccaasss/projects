10a3042d8d5633e0c14009f48edf115e867c9dae
Luccaasss <lucasrodriguesprb@gmail.com>
2022-01-03 23:35:32 -0300

    just tests react-tests

--- react-tests/src/App.js
+++ react-tests/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [names, setNames] = useState(['João', 'Maria', 'Pedro', 'Mateus', 'Silva'])
+  const initialNames = ['João', 'Maria', 'Pedro', 'Mateus', 'Silva']
+
+  function plusOne() {
+    setCounter(counter + 1)
+  }
+
+  function minusOne() {
+    setCounter(counter - 1)
+  }
+
+  function resetCounter() {
+    setCounter(0)
+    setNames(initialNames)
+  }
+
+  function deleteName(element) {
+    const prevNames = [...names];
+    setNames(prevNames.filter(e => e !== element))
+  }
+
+  function Namelist({element}) {
+    return (
+      <li>{element} <button onClick={() => deleteName(element)}>x</button></li>
+    )
+  }
+
+  function NewNames() {
+    const [newName, setNewName] = useState();
+    const oldnames = [...names]
+    return (
+      <div>
+
+        <label htmlFor="name">Add new name</label>
+        <input id="name" name="name" type='text' onChange={(e) => {
+          setNewName(e.target.value)
+        }}/>
+        <button onClick={() => setNames([...oldnames, newName]) }>Add Name</button>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter</h1>
+      <h3>{counter}</h3>
+      <button onClick={plusOne}>+</button>
+      <button onClick={minusOne}>-</button>
+      <button onClick={resetCounter}>reset</button>
+      {names.map(e => <Namelist key={e} element={e}/>)}
+      <NewNames />
     </div>
   );
 }
+
+
 
 export default App;
